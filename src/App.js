5c763ee7a0a37a9db82aa13eb9811863ccd5f092
Vince Emond <vince.emond@icloud.com>
2022-03-21 13:31:07 -0700

    Completed exercise

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import TextRepeaterButton from './components/TextRepeaterButton';
 
 
 function App() {
+  const [light, setLight] = useState('off');
+  const dark = (light === 'off' ? 'dark' : '');
+  const switchLight = () => setLight(light === 'on'? 'off':'on');
+
   return (
-    <div className={`App`}>
+    <div className={`App ${dark}`}>
     <h1>Fancy Buttons!</h1>
     <section>
       <AngryButton />
       <CounterButton />
-      <LightSwitchButton />
+      <LightSwitchButton light={light} setLight={setLight} switchLight={switchLight} />
       <TextRepeaterButton />
     </section>
   </div>
--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 
 function TextRepeaterButton(){
-  const [repetitions, setRepetitions] = useState();
+  const [repetitions, setRepetitions] = useState(1);
+  const textArray = [];
+  for (let i = 0; i < repetitions; i++) {
+    textArray.push(<span key={i}>I like this text</span>);
+  }
+
+  const clickHandler = () => {
+    setRepetitions(repetitions +1);
+  }
+
   return (
-    <button className="TextRepeaterButton">
-      <span>I like this text</span>
+    <button onClick={clickHandler} className="TextRepeaterButton">
+      {textArray}
     </button>
   );
 }
