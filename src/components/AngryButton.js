a18b4fd950314e413a4834fb85f7fea8d3b2ce3e
Vince Emond <vince.emond@icloud.com>
2022-03-21 10:09:06 -0700

    Updated readme

---
+++ src/components/AngryButton.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function AngryButton(){
+  const 
+  return (
+    <button className="AngryButton">
+      {/* If you have NOT reached the maximum */}<span>Don't click me too much! </span>
+      {/* If you HAVE reached the maximum */}<span>Rawr!</span>
+    </button>
+  );
+}
+
+export default AngryButton;
